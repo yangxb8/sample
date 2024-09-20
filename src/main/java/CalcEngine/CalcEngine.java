5d0d782b3ccdadf8e6ee88f9180847342b8ac742
yangxb10 <yangxb10@gmail.com>
2024-09-20 21:28:49 +0900

    new commit

---
+++ src/main/java/CalcEngine/CalcEngine.java
@@ -0,0 +1,4 @@
+package CalcEngine;
+
+public class CalcEngine {
+}
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+class Main2 {
+    public static void main (String[] args) {
+        System.out.println("In Main2");
+    }
+}
+
+class Main3 {
+
+}
